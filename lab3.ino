ee1a38d817497044b79247484c16e31a85e800d3
Ben Kahle <ben@kahle.org>
2014-10-14 00:59:45 -0400

    working code and motor stand image.

--- lab3.ino
+++ lab3.ino
@@ -10,11 +10,10 @@ int historyCount = 0;
 int motorDir = 1; //1 = forward, -1 = reverse
 int encoderPos = 0; //0 = light, 1 = dark
 const int encoderLightCutoff = 200;
-const int encoderRes = 20; // Resolution of encoder light change
+const int encoderRes = 9; // Resolution of encoder light change
 int command = 0;
-const float kp = 1.2;
-const float kd = 0;
-const float ki = 0.0002;
+float kp;
+float ki;
 int commandMode = 0; //0 = step, 1 = sweep
 int positionGoal = 0;
 int error = 0;
@@ -22,7 +21,11 @@ int motorCommand = 0;
 long errorSum = 0;
 int timeInCurrentPos = 0;
 int timeInLastPos = 0;
-const int loopDelay = 2;
+const int loopDelay = .2;
+const float sweepFreq = 1; //Sweeps per second
+int loopCounter = 0;
+float loopsPerSecond = 1000/loopDelay;
+float loopsPerSweepStep = loopsPerSecond/120;
 
 static int sinWave[120] = {
     0x7ff, 0x86a, 0x8d5, 0x93f, 0x9a9, 0xa11, 0xa78, 0xadd, 0xb40, 0xba1,
@@ -37,7 +40,7 @@ static int sinWave[120] = {
     0x0, 0x2, 0xb, 0x19, 0x2c, 0x45, 0x64, 0x87, 0xb1, 0xdf,
     0x112, 0x14a, 0x187, 0x1c8, 0x20d, 0x257, 0x2a5, 0x2f6, 0x34c, 0x3a4,
     0x3ff, 0x45d, 0x4be, 0x521, 0x586, 0x5ed, 0x655, 0x6bf, 0x729, 0x794
-}
+};
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *motor = AFMS.getMotor(1);
@@ -59,45 +62,49 @@ void powerMotor(int speed) {
   motor->setSpeed(abs(speed));
 }
 
-void step() {
+void setMotor(float kp, float ki) {
   error = positionGoal - motorPos;
   errorSum += error;
-  int pTerm = error*kp;
+  int pTerm = error+kp;
   int iTerm = errorSum*ki;
   motorCommand = min(pTerm + iTerm, 255);
   powerMotor(motorCommand);
 }
 
-void sweep() {
+void step() {
+  kp = 1.2;
+  ki = 0.0007;
+  setMotor(kp, ki);
+}
 
+void sweep(int sweepPos, int command) {
+  kp = 165;
+  ki = 0.008;
+  int waveIndex = sweepPos/loopsPerSweepStep;
+  float fractionalPos = (float)sinWave[waveIndex]/4096;
+  positionGoal = fractionalPos*command;
+  setMotor(kp, ki);
 }
 
 void adjustPosition(int encPos) {
-  //timeInLastPos = timeInCurrentPos;
-  //timeInCurrentPos = 0;
   motorPos += encoderRes*motorDir; //incr. or decr. pos based on direction
   encoderPos = encPos;
-  //motorPosHist[historyCount] = motorPos;
-  //historyCount = historyCount++%10;
-  //Serial.println(motorPos);
 }
 
 void printInfo() {
-  Serial.print("Co: ");
-  Serial.print(command);
-  Serial.print(", Mode: ");
-  Serial.print(commandMode);
-  Serial.print(", Pos: ");
-  Serial.print(motorPos);
-  Serial.print(", Goal: ");
+  // Serial.println(sensorValue);
   Serial.print(positionGoal);
-  Serial.print(", SensorRead: ");
-  Serial.println(encoderPos);
-  Serial.print(", MoCom: ");
+  Serial.print(",");
+  Serial.print(motorPos);
+  Serial.print(",");
+  Serial.print(error);
+  Serial.print(",");
   Serial.println(motorCommand);
 }
 
 void loop() { 
+  // motor->run(FORWARD);
+  // motor->setSpeed(22);
   sensorValue = analogRead(sensorPin);
   if (sensorValue < encoderLightCutoff && encoderPos == 1) {
     adjustPosition(0);
@@ -107,19 +114,23 @@ void loop() {
   if (Serial.available() > 0) {
     command = Serial.parseInt();
     errorSum = 0;
+    //Positive command = Go to <commmand> degrees
     if (command >= 0) {
       commandMode = 0;
       positionGoal = command;
+      //Negative command = Sweep from 0 to <command> degrees
     } else {
       commandMode = 1;
+      //Flip command to positive degrees
+      command = -command;
     }
   }
   if (!commandMode) {
     step();
   } else {
-    sweep();
+    sweep(loopCounter, command);
   }
-  //timeInCurrentPos += loopDelay;
   printInfo();
+  loopCounter = ++loopCounter%((int)(121*loopsPerSweepStep));
   delay(loopDelay);
 }
