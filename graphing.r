a9dd2c5164b9a40f0078513936b27b8145070bc0
Ry Horsey <henry.horsey@students.olin.edu>
2014-10-14 01:59:30 -0400

    Added comments

--- graphing.r
+++ graphing.r
@@ -1,6 +1,9 @@
+#Post processing script for POE Fall 2013 Lab 3
+#Initialize the required packages
 require(ggplot2)
 require(grid)
 
+#Load in data for the sweep function and reformat it for ggplot2
 setwd("~/gitRepos/fall2014/poe/labThree/poe_motorcontroller")
 sweep_df = read.csv(file = 'sweep.csv', header = FALSE)
 colnames(sweep_df) = c('set_pos', 'meas_pos', 'error', 'signal')
@@ -16,12 +19,14 @@ for(j in 1:ncol(sweep_df)){
     ploting_df[(i+init_ind),'type'] = colnames(sweep_df)[j]
   }
 }
+#Plot the sweep data
 ggplot(data = ploting_df, aes(x = time, y = value, colour = factor(type)))+
   geom_line(size = rel(1))+
   labs(x = "Time (s)", y = "Degrees or Arduino Out", title = "Sinusoidal Following of DC Motor")+
   theme(legend.text=element_text(size=rel(1.25)),axis.title.x = element_text(size=rel(1)), axis.title.y = element_text(size=rel(1)), title = element_text(size=rel(1.75)), axis.text.x = element_text(size=rel(1.5)), axis.text.y = element_text(size=rel(1.75)))+
   scale_color_manual(name = 'Signal Type', values = c("Red", "Blue", "Black", "DarkGreen"), labels = c('Error', 'Measured Position', 'Set Position', 'Signal to Motor'))
 
+#Load in and reformat for ggplot2 the step data
 step_df = read.csv(file = 'step90.csv', header = FALSE)
 step_df = step_df[1:1000,]
 colnames(step_df) = c('set_pos', 'meas_pos', 'error', 'signal')
@@ -37,12 +42,14 @@ for(j in 1:ncol(step_df)){
     ploting_df[(i+init_ind),'type'] = colnames(step_df)[j]
   }
 }
+#Plot the step function
 ggplot(data = ploting_df, aes(x = time, y = value, colour = factor(type)))+
   geom_line(size = rel(1))+
   labs(x = "Time (s)", y = "Degrees or Arduino Out", title = "Step Responce of DC Motor")+
   theme(legend.text=element_text(size=rel(1.25)),axis.title.x = element_text(size=rel(1)), axis.title.y = element_text(size=rel(1)), title = element_text(size=rel(1.75)), axis.text.x = element_text(size=rel(1.5)), axis.text.y = element_text(size=rel(1.75)))+
   scale_color_manual(name = 'Signal Type', values = c("Red", "Blue", "Black", "DarkGreen"), labels = c('Error', 'Measured Position', 'Set Position', 'Signal to Motor'))
 
+#Load the encoder data and plot
 encoder = read.csv(file = 'encoder.csv', header = FALSE)
 plot(x = c(1:nrow(encoder)), y = encoder[1:nrow(encoder),], xlab = 'Datapoint', ylab = 'IR Sensor Responce', main = 'Encoder Calibration Plot')
 lines(x = c(0, 3450), y = c(70, 70), col = 'blue', lwd = 5)
